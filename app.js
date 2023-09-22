2bf013333c1366fc7ec18c86ff0ff07bed802eb4
Shanda Shaw <srh0029@hotmail.com>
2023-09-22 16:01:34 -0500

    Calculator working for 2 & more operators.

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
+const Output = document.querySelector("#outputText")
+
 let numInput = ""
 let leftSide = 0
 let operator = ""
 let rightSide = 0
+let operatorCount = 0
 
+Output.innerText = ""
 
 document.querySelector(".container").onclick = function(e) {
     if ((e.target.id) === "1" || (e.target.id) === "2" || (e.target.id) === "3" || (e.target.id) === "4" || (e.target.id) === "5" || (e.target.id) === "6" || (e.target.id) === "7" || (e.target.id) === "8" || (e.target.id) === "9" || (e.target.id) === "0") {
         numInput+=(e.target.id)
         console.log(numInput)
+        //** POPULATE NUMINPUT TO HTML OUTPUT */
+        Output.innerText = numInput
     } else if ((e.target.id) === "+" || (e.target.id) === "-" || (e.target.id)=== "*" || (e.target.id)=== "/") {
-        operator = (e.target.id)
-        leftSide = parseInt(numInput)
+        operatorCount = operatorCount + 1
+        if (operatorCount > 1 ) {
+            rightSide = parseInt(numInput)
+            
+            // Ready to evaluate your expression and set it to leftSide, then set rightSide to 0.
+            if (operator === "+"){
+                leftSide = leftSide + rightSide
+            } else if (operator === "-") {
+                leftSide = leftSide -rightSide
+            } else if (operator === "*") {
+                leftSide = leftSide * rightSide
+            } else {
+                leftSide = leftSide / rightSide
+            }
+
+            rightSide = 0
+            operator = (e.target.id)
+
+        } else {
+            operator = (e.target.id)
+            leftSide = parseInt(numInput)
+        }
         numInput = ""
-        console.log(operator +" operator , leftSide "+ leftSide+", numInput "+numInput)
         
     } else if ((e.target.id) === "=" ){
         rightSide=parseInt(numInput)
@@ -26,8 +51,20 @@ document.querySelector(".container").onclick = function(e) {
             leftSide = leftSide / rightSide
         }
         console.log(leftSide)
+        numInput = ""
+        Output.innerText = leftSide
+        //**POPULATE leftSide TO HTML CALC
         
     } else {
+        numInput = ""
+        leftSide = 0
+        operator = ""
+        rightSide = 0
+        operatorCount = 0
+
+        Output.innerText = ""
+        
+        /**POPULATE LS TO HTML CALC */
         console.log("clear")
     }
 }
