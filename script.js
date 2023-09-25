f5238754e788be471d73d74dca6286f29067d3d8
rayasF <rayas.f@gmail.com>
2023-09-24 21:39:44 -0500

    1stdraft

---
+++ script.js
@@ -0,0 +1,30 @@
+const screenDisplay = document.querySelectorAll('.screen')
+const buttons = document.querySelectorAll('button')
+
+let calculation = []
+let accumulativeCalculation
+
+function calculate(button) {
+ const value = button.textContent  
+ 
+//  if(value ==="CLEAR"){
+//     calculation =[]
+//     screenDisplay.textContent ='.'
+//  }else if (value === "=") {
+//     screenDisplay.textContent = eval(accumulativeCalculation)
+
+    console.log(calculation)
+//  }
+    
+
+    calculation.push(value)
+    accumulativeCalculation = calculation.join('')
+    screenDisplay.textContent = accumulativeCalculation
+    
+}
+
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
+
+
+
